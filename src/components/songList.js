2f6bfff82a20c711f80164c1c9e40b54a6e7a539
Gee <glynester@hotmail.com>
2022-03-24 17:22:35 +0000

    Select button linked to selectSong()

--- src/components/songList.js
+++ src/components/songList.js
@@ -2,6 +2,7 @@
 import React, {Component} from 'react';
 // connect - Component made by react-redux library.
 import { connect } from 'react-redux';
+import { selectSong } from '../actions';
 
 // class SongList extends React.Component {
 class SongList extends Component {
@@ -10,7 +11,10 @@ class SongList extends Component {
       return (
         <div className='item' key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">
+            <button 
+              className="ui button primary"
+              onClick={()=>this.props.selectSong(song)}
+            >
               Select
             </button>
           </div>
@@ -34,12 +38,21 @@ class SongList extends Component {
 }
 
 // By convention we name this mapStateToProps.
+// Runs every time we change our REDUX state or we run our reducers and produce some new state objects
 const mapStateToProps=(state)=>{
-  console.log(state);
+  console.log("state=>",state);
   return {songs: state.songs};    // Will show up as props inside our component.
 }
 
 // Call connect and pass in component as 2nd function call.
-export default connect(mapStateToProps)(SongList);
+// export default connect(mapStateToProps)(SongList);
+// Pass in action creator as 2nd argument.
+// connect function will take that selectSong action creator and pass it into our component as a prop.
+// Note that we don't just call the action creator directly when the button gets clicked
+export default connect(mapStateToProps, { selectSong })(SongList);
+
+// Redux only detects action creator "selectSong" because of the way it is set up above.
+// This setup calls the despatch function for us - we don't need to manually do this.
+// Always pass an action creator into the connect function.
 
 
